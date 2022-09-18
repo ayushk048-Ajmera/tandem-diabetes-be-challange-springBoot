b99959a792b11c5f50c6e75c8ad31181b7df04f0
ayushk048-Ajmera <ayush.kholiya@ajmerainfotech.com>
2022-09-18 19:24:42 +0530

    Optimized imports

--- src/main/java/com/backendchallenge/services/IUserService.java
+++ src/main/java/com/backendchallenge/services/IUserService.java
@@ -2,7 +2,6 @@ package com.backendchallenge.services;
 
 import com.backendchallenge.dtos.UserRequestDTO;
 import com.backendchallenge.dtos.UserResponseDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
